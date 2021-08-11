97fd1545e6e48b7806ef09d983c39e41d3bf0d9d
Yannikpy <43669836+Yannikpy@users.noreply.github.com>
2021-08-11 16:10:21 +0200

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -325,11 +325,29 @@ if (!isMobile) {
 const swiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	direction: 'horizontal',
-	scrollbar: {
-		el: ".swiper-scrollbar",
-	  },
 	loop: true,  
   });
+
+  var startScroll, touchStart, touchCurrent;
+  swiper.slides.on('touchstart', function (e) {
+	  startScroll = this.scrollTop;
+	  touchStart = e.targetTouches[0].pageY;
+  }, true);
+  swiper.slides.on('touchmove', function (e) {
+	  touchCurrent = e.targetTouches[0].pageY;
+	  var touchesDiff = touchCurrent - touchStart;
+	  var slide = this;
+	  var onlyScrolling = 
+			  ( slide.scrollHeight > slide.offsetHeight ) &&
+			  (
+				  ( touchesDiff < 0 && startScroll === 0 ) ||
+				  ( touchesDiff > 0 && startScroll === ( slide.scrollHeight - slide.offsetHeight ) ) ||
+				  ( startScroll > 0 && startScroll < ( slide.scrollHeight - slide.offsetHeight ) )
+			  );
+	  if (onlyScrolling) {
+		  e.stopPropagation();
+	  }
+  }, true);
 
 swiper.on('slideChange', function () {
 		//console.log("prev:" + n)
