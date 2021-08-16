23f77f149e6918c20900242dda84714ee83285d2
Yannikpy <43669836+Yannikpy@users.noreply.github.com>
2021-08-16 19:34:14 +0200

    Add files via upload

--- data/text.js
+++ data/text.js
@@ -1,29 +1,20 @@
-var logos = "<div class='buttoncontainerInsta'> "
-	logos = logos + "<a target='_blank' rel='noopener noreferrer'  href='https://www.instagram.com/reclaimaward/'> <img id='insta' src='symbols/Instagram.svg'></img></a>" 
-	logos = logos + "<a target='_blank' rel='noopener noreferrer'  href='https://www.reclaim-award.org/'><img id='wwww' src='symbols/wwww.svg'></img> </a>"
-	logos = logos + "</div>"
-	
-
-var infotext_de = "<div class='myContent'> " + buttons + " <p4>Яeclaim Award 2021 <br> Kunst auf Werbeflächen: Die Stadt wird zum Museum! <br> <br> <br> <br> Entdecke jetzt 44 Kunstwerke im urbanen Raum Kölns. Folge der Route und finde die 44 Perspektiven der internationalen Künstler:innen im Großformat auf Werbeplakaten!"
-    infotext_de += "<br> <br> <br> Das Reclaim Kollektiv veranstaltet einen Award, der Kunst im öffentlichen Raum zeigt. Plakatflächen werden angemietet und Künstler:innen als Ausstellungsfläche zur Verfügung gestellt. Der Reclaim Award findet einmal jährlich in Köln statt. Mit dem Award wollen wir dazu beitragen, den öffentlichen Raum kulturell aufzuwerten und das Stadtbild Kölns attraktiver zu gestalten. <br><br> " + logos
-    infotext_de += "<br> <br> <br> Weitere Informationen findest du auf Instagram und auf unserer Website: <br><br> " + logos
-    infotext_de += " <br> <br> Dir gefällt unser Projekt und du hast Spaß an der Tour? Dann unterstütze uns mit einer kleinen Spende via Paypal und mach dieses Projekt auch weiterhin möglich.  <br></p4>"
-    infotext_de += '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick" /><input type="hidden" name="hosted_button_id" value="8EYP2LJVHHKBA" /><input type="image" src="symbols/Reclaim-Spende.png" border="0" class="donateIMG" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" /></form>'
-    infotext_de += "<p4> <br> Herzlichen Dank an alle Unterstützer:innen! <br>"
-    infotext_de += "<br> <br> Ein besonderer Dank geht an Yannik Venohr der mit uns diese Karte realisiert hat. Verwendet wurde die Bibliothek <a target='_blank' rel='noopener noreferrer' href='https://leafletjs.com'>Leaflet.</a> Hintergrundkarte auf der Basis von <a target='_blank' rel='noopener noreferrer'   href='http://stamen.com' >Stamen</a> und <a target='_blank' rel='noopener noreferrer'  href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>, <a target='_blank' rel='noopener noreferrer' href='http://creativecommons.org/licenses/by/3.0'>Creative Commons 3.0.</a>"
-    infotext_de += "<br> <br> <br>Außerdem möchten wir Yale Sevis und Elisabeth Rosenberg danken, die dem Audio-Guide ihre Stimmen geliehen haben. Ebenso Tim Junge und Mathieu Mardou für das Aufnehmen und Mastern ihrer Stimmen.</p4>"
-
-var infotext_en = "<div class='myContent'> " + buttons + " <p4>Яeclaim Award 2021 <br> Art on billwords: The City transformed into an museum!<br> <br> <br> <br>Discover now 44 Artworks in the urban space of Cologne. Follow this Route and find 44 perspectives of international artists in big scale – displayed on advertisment billboards."
-    infotext_en += "<br> <br> <br> Further information you will find on our instagram or website: <br><br> " + logos
-    infotext_en += " <br> <br> You like our project and enjoy our tour? Then support us with a small donation via Paypal and help us to reclaim billboards in the coming year as well! :) <br></p4>"
-    infotext_en += '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick" /><input type="hidden" name="hosted_button_id" value="8EYP2LJVHHKBA" /><input type="image" src="symbols/Reclaim-Donation.png" border="0" class="donateIMG" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" /></form>'
-    infotext_en += "<p4> <br> Many thanks to all supporters! <br>"
-    infotext_en += "<br> <br> A special thanks goes to Yannik Venohr who realised this map with us. It uses the <a target='_blank' rel='noopener noreferrer' href='https://leafletjs.com'>Leaflet</a> library. The map is based on <a target='_blank' rel='noopener noreferrer'   href='http://stamen.com' >Stamen</a> and <a target='_blank' rel='noopener noreferrer'  href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>, <a target='_blank' rel='noopener noreferrer' href='http://creativecommons.org/licenses/by/3.0'>Creative Commons 3.0.</a>"
-    infotext_en += "<br> <br> <br> We would also like to thank Vita Tepel and Savannah Eisert, who lent their voices to the audio guide. Also thanks to Emmanuel Homberger for the mastering of their voices.</p4>"
-    
+  
 var landingtext_en = "<p4>This is the landingbox.</p4>"
-    landingtext_en += "<br> <br> <br> Further information you will find on our instagram or website: <br><br> " + logos
+    landingtext_en += "<br> <br> <br> Further information you will find on our instagram or website: <br><br> "
     
 var landingtext_de = "<p4>Dies ist die Landing-Box.</p4>"
-    landingtext_de += "<br> <br> <br> Weitere Informationen findest du auf Instagram und auf unserer Website: <br><br> " + logos
+    landingtext_de += "<br> <br> <br> Weitere Informationen findest du auf Instagram und auf unserer Website: <br><br> " 
    
+var infotext_de = {
+    "title": " Яeclaim Award 2021",    
+    "text1": "Kunst auf Werbeflächen: Die Stadt wird zum Museum! <br> Entdecke jetzt 44 Kunstwerke im urbanen Raum Kölns. Folge der Route und finde die 44 Perspektiven der internationalen Künstler:innen im Großformat auf Werbeplakaten! <br> <br> Das Reclaim Kollektiv veranstaltet einen Award, der Kunst im öffentlichen Raum zeigt. Plakatflächen werden angemietet und Künstler:innen als Ausstellungsfläche zur Verfügung gestellt. Der Reclaim Award findet einmal jährlich in Köln statt. Mit dem Award wollen wir dazu beitragen, den öffentlichen Raum kulturell aufzuwerten und das Stadtbild Kölns attraktiver zu gestalten. <br> <br> Weitere Informationen findest du auf Instagram und auf unserer Website:  ",
+    "text2": "Dir gefällt unser Projekt und du hast Spaß an der Tour? Dann unterstütze uns mit einer kleinen Spende via Paypal und mach dieses Projekt auch weiterhin möglich. ",
+    "text3": "Herzlichen Dank an alle Unterstützer:innen!  <br> <br>A Ein besonderer Dank geht an Yannik Venohr der mit uns diese Karte realisiert hat. Verwendet wurde die Bibliothek Leaflet. Hintergrundkarte auf der Basis von Stamen und OpenStreetMap, Creative Commons 3.0.<br> <br> Außerdem möchten wir Yale Sevis und Elisabeth Rosenberg danken, die dem Audio-Guide ihre Stimmen geliehen haben. Ebenso Tim Junge und Mathieu Mardou für das Aufnehmen und Mastern ihrer Stimmen."
+}
+
+var infotext_en = {
+    "title": "Яeclaim Award 2021",    
+    "text1": "Art on billwords: The City transformed into an museum! <br> Discover now 44 Artworks in the urban space of Cologne. Follow this Route and find 44 perspectives of international artists in big scale – displayed on advertisment billboards. <br> <br> Reclaim is a collective of individuals initiating an award in Cologne that redefines public space as a place for art. We rent billboards and make them available to artists as their individual exhibition space. <br> By creating this award, the Reclaim collective contributes to the city’s appeal and enriches its cultural life. The prize is to be awarded regularly once a year in Cologne. Billboards will be reclaimed from their mere commercial use by turning them into spaces of artistic expression and reflection. Their panels become experimental spaces. Depicting diversity, they are inspiring dispute about the use of public space and serve the individual expression of the independent artists. <br> <br> Further information you will find on our instagram or website: ",
+    "text2": "You like our project and enjoy our tour? Then support us with a donation via Paypal and help us to continue next year! :)",
+    "text3": "Many thanks to all supporters! <br> <br>A special thanks goes to Yannik Venohr who realised this map with us. It uses the Leaflet library. The map is based on Stamen and OpenStreetMap, Creative Commons 3.0. <br> <br> We would also like to thank Yale Sevis and Elisabeth Rosenberg who lent their voices to the audio guide. Also thanks to Tim Junge and Mathieu Mardou for recording and mastering their voices."
+}
